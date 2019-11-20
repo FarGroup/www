c0803bcb2c22a01f1d2c56ad0944a8624df4b0f1
Alex Alabuzhev <alabuzhev@gmail.com>
2019-11-20 18:26:45 +0000

    Fix OS page index, transparent logo background

--- site/inc/common.en.php
+++ site/inc/common.en.php
@@ -3,7 +3,7 @@
 $header_pages = array ("index.php?l=en", "screenshots.php?l=en", "download.php?l=en", "plugins.php?l=en", "problems.php?l=en", "links.php?l=en", "https://plugring.farmanager.com/index.php?l=en", "https://enforum.farmanager.com", "opensource.php?l=en", "license.php?l=en");
 $header_pages_strip = array ("index.php", "screenshots.php", "download.php", "plugins.php", "problems.php", "links.php", "https://plugring.farmanager.com/index.php", "https://enforum.farmanager.com", "opensource.php", "license.php");
 $header_descriptions = array ("main", "screenshots", "download", "plugins", "problems", "links", "plugring", "forum", "open&nbsp;source", "license");
-$current_page_opensource = 9;
+$current_page_opensource = 8;
 
 function do_footer ()
 {
--- site/inc/common.ru.php
+++ site/inc/common.ru.php
@@ -3,7 +3,7 @@
 $header_pages = array ("index.php?l=ru", "screenshots.php?l=ru", "download.php?l=ru", "plugins.php?l=ru", "problems.php?l=ru", "links.php?l=ru", "https://plugring.farmanager.com/index.php?l=ru", "https://forum.farmanager.com", "opensource.php?l=ru", "license.php?l=ru");
 $header_pages_strip = array ("index.php", "screenshots.php", "download.php", "plugins.php", "problems.php", "links.php", "https://plugring.farmanager.com/index.php", "https://forum.farmanager.com", "opensource.php", "license.php");
 $header_descriptions = array ("главная", "галерея", "загрузить", "плагины", "проблемы", "ссылки", "plugring", "форум", "исходный&nbsp;код", "лицензия");
-$current_page_opensource = 9;
+$current_page_opensource = 8;
 
 function do_footer ()
 {
