247b3fd5ddd04cf99ba082847b84e00430c930a3
anatoly techtonik <techtonik@gmail.com>
2020-10-25 22:07:23 +0300

    Remove Accept-Charset detection code
    https://hsivonen.fi/accept-charset/

--- site/getlang.php
+++ site/getlang.php
@@ -20,31 +20,6 @@ function GetClientLang()
 
     $lang = !empty($_SERVER["HTTP_ACCEPT_LANGUAGE"]) ? $_SERVER["HTTP_ACCEPT_LANGUAGE"] : "";
 
-    if ($lang=="")
-    {
-        $lang = !empty($_SERVER["HTTP_ACCEPT_CHARSET"]) ? $_SERVER["HTTP_ACCEPT_CHARSET"] : "";
-
-        if (strpos($lang,"windows-1251")!==FALSE) // тут надо именно !==
-            $lang="ru";
-        else if (strpos($lang,"iso-8859-5")!==FALSE)
-            $lang="ru";
-        else if (strpos($lang,"ibm866")!==FALSE)
-            $lang="ru";
-        else if (strpos($lang,"koi8-r")!==FALSE)
-            $lang="ru";
-        else if (strpos($lang,"mac")!==FALSE)
-            $lang="ru";
-        else
-            $lang="en";
-    }
-    else
-    {
-        if (strpos($lang,"ru")!==FALSE)
-            $lang="ru";
-        else
-            $lang="en";
-    }
-
-    return $lang;
+    return stristr($lang,"ru") ? "ru" : "en";
 }
 ?>
